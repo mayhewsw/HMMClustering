7ca67e9a2224ed032d5652f4f42212fd2a11b826
Nicholas Kamper <kampernj@rose-hulman.edu>
2012-02-22 18:25:40 -0500

    * Added new clustering algorithm

--- src/clusteralgorithm.py
+++ src/clusteralgorithm.py
@@ -12,7 +12,65 @@ def score_oro_string(iline, jline, N):
 	scores += [NGram.compare(" ".join(iline.split(" :::: ")[:3]), " ".join(jline.split(" :::: ")[:3]), N=N) * 10]
 	return sum([log(x) if x > 0 else -10 for x in scores])
 
-def build_clusters(inlines, threshold=0.3, N=3, scorer=simple_score_strings):
+def backoff_score_strings(iline, jline, N, T=0.0):
+	iString = " ".join(iline.split(" :::: ")[:3])
+	jString = " ".join(jline.split(" :::: ")[:3])
+
+	score = -1
+	while score <= T and N >= 1:
+		score = NGram.compare(iString, jString, N=N)
+		N = N - 1
+
+	return score
+
+def cumulative_score_strings(iline, jline, N):
+	iString = " ".join(iline.split(" :::: ")[:3])
+	jString = " ".join(jline.split(" :::: ")[:3])
+
+	score = 0
+	while N >= 1:
+		score += (NGram.compare(iString, jString, N=N)) #* N)
+		N = N - 1
+
+	return score
+
+def build_multiclusters(inlines, threshold=0.05, N=4):
+	clusters = []
+	ignoreus = []
+
+	for i, iline in enumerate(inlines):
+		if i in ignoreus:
+			continue
+
+		iString = " ".join(iline.split(" :::: ")[:3])
+
+		ignoreus.append(i)
+
+		icluster = {}
+		icluster[iline] = -1
+		iModel = NGram(iString)
+
+		for j in range(i, len(inlines)):
+			if j in ignoreus:
+				continue
+		
+			jline = inlines[j]
+			jString = " ".join(jline.split(" :::: ")[:3])
+		
+			results = iModel.search(jString)
+			score = sum([y for x,y in results]) / len(results) \
+					if len(results) > 0 else 0.0
+			print score
+
+			if score > threshold:
+				icluster[jline] = score
+				iModel.add(jString)
+				ignoreus.append(j)
+
+		clusters.append(icluster)
+	return clusters
+
+def build_clusters(inlines, threshold=0.3, N=4, scorer=simple_score_strings):
 	clusters = []
 	ignoreus = []
 
@@ -35,6 +93,7 @@ def build_clusters(inlines, threshold=0.3, N=3, scorer=simple_score_strings):
 			jString = " ".join(jline.split(" :::: ")[:3])
 			
 			score = scorer(iline, jline, N)
+			print score
 
 			if score > threshold:
 				icluster[jline] = score
@@ -51,7 +110,7 @@ def write_clusters(outfile, clusters):
 
 def cluster_to_file(infilename, outfilename, *args, **kwargs):
 	with open(infilename, "r") as infile:
-		clusters = build_clusters(infile.readlines(), *args, **kwargs)
+		clusters = build_multiclusters(infile.readlines(), *args, **kwargs)
 	with open(outfilename, "w") as outfile:
 		write_clusters(outfile, clusters)
 
--- src/multicluster.py
+++ src/multicluster.py
@@ -1,10 +1,23 @@
 from clusteralgorithm import cluster_to_file
+from multiprocessing import Pool
+
+
+def cluster(args):
+	infilename, outfilename, threshold, n = args
+	print "Starting cluster n=%s, thres=%s" % (n, threshold)
+	cluster_to_file(infilename, outfilename, threshold=threshold, N=n)
+	return 1
+
+#def cluster(args):
+	#print args
 
 if __name__ == "__main__":
 	infilename = "randomized_clusters6.txt"
+	p = Pool()
+	r = p.map_async(cluster, ((infilename, ("out/cumulative/auto_%s_%s.txt" %
+		(thres, n)), thres, n) for thres in
+		[x*0.1 for x in xrange(2, 8)] for n in xrange(2, 15)))
+	print "Finished %s clusters" % r.get()
+	p.close()
+	p.join()
 
-	for n in xrange(1, 7):
-		for thres in [x*0.1 for x in xrange(2, 9)]:
-			print "Running for n=%s, thres=%s" % (n, thres)
-			outfilename = "out/PyNGram_auto_%s_%s.txt" % (n, thres)
-			cluster_to_file(infilename, outfilename, threshold=thres, N=n) 
